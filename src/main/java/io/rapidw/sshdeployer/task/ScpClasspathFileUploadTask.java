2bfe7bad7c711d072c03bce3b5990fce33ccd089
pvtyuan <pvtyuan@foxmail.com>
2020-03-14 12:32:09 +0800

    bug fix

--- src/main/java/io/rapidw/sshdeployer/task/ScpClasspathFileUploadTask.java
+++ src/main/java/io/rapidw/sshdeployer/task/ScpClasspathFileUploadTask.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2020 Rapidw
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,6 +23,7 @@ import org.apache.sshd.client.scp.ScpClientCreator;
 import org.apache.sshd.client.session.ClientSession;
 
 import java.io.ByteArrayOutputStream;
+import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Objects;
@@ -40,15 +41,12 @@ public class ScpClasspathFileUploadTask implements SshTask {
     @Override
     public void execute(ClientSession session, SshDeployerOptions options, ByteArrayOutputStream out, ByteArrayOutputStream err) {
         try {
-            Path local = Paths.get(Objects.requireNonNull(this.getClass().getClassLoader().getResource(localPath)).toURI());
-        } catch (Exception e) {
-            throw new SshDeployerException(e);
-        }
+            URL url = this.getClass().getClassLoader().getResource(localPath);
+            Path local = Paths.get(Objects.requireNonNull(this.getClass().getResource(localPath)).toURI());
+            ScpClientCreator creator = ScpClientCreator.instance();
+            ScpClient client = creator.createScpClient(session);
 
-        ScpClientCreator creator = ScpClientCreator.instance();
-        ScpClient client = creator.createScpClient(session);
-        try {
-            client.upload(localPath, remotePath, ScpClient.Option.PreserveAttributes);
+            client.upload(local, remotePath, ScpClient.Option.PreserveAttributes);
         } catch (Exception e) {
             throw new SshDeployerException(e);
         }
